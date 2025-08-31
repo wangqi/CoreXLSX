55c14c64ccfc4634473f40ffc1b4c84fad631be6
wangqi <wang.qi@qikuyx.com>
2025-08-31 10:13:02 -0400

    Forward compatible with Excel schema in Sources/CoreXLSX/Relationships.swift

--- Sources/CoreXLSX/Relationships.swift
+++ Sources/CoreXLSX/Relationships.swift
@@ -31,105 +31,142 @@ public struct Relationships: Codable, Equatable {
  wouldn't need to handle relationships directly.
  */
 public struct Relationship: Codable, Equatable {
-  public enum SchemaType: String, Codable {
-    case calcChain =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      calcChain
-      """
-    case officeDocument =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      officeDocument
-      """
-    case extendedProperties =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      extended-properties
-      """
-    case packageCoreProperties =
-      """
-      http://schemas.openxmlformats.org/package/2006/relationships/metadata/\
-      core-properties
-      """
-    case coreProperties =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      metadata/core-properties
-      """
-    case connections =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      connections
-      """
-    case worksheet =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      worksheet
-      """
-    case chartsheet =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      chartsheet
-      """
-    case sharedStrings =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      sharedStrings
-      """
-    case styles =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      styles
-      """
-    case theme =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      theme
-      """
-    case pivotCache =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      pivotCacheDefinition
-      """
-    case metadataThumbnail =
-      """
-      http://schemas.openxmlformats.org/package/2006/relationships/metadata/\
-      thumbnail
-      """
-    case customProperties =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      custom-properties
-      """
-    case externalLink =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      externalLink
-      """
-    case customXml =
-      """
-      http://schemas.openxmlformats.org/officeDocument/2006/relationships/\
-      customXml
-      """
-    case person =
-      """
-      http://schemas.microsoft.com/office/2017/10/relationships/\
-      person
-      """
-    case webExtensionTaskPanes =
-      """
-      http://schemas.microsoft.com/office/2011/relationships/\
-      webextensiontaskpanes
-      """
-    case googleWorkbookMetadata =
-      """
-      http://customschemas.google.com/relationships/workbookmetadata
-      """
-    case purlOCLC =
-      """
-      http://purl.oclc.org/ooxml/officeDocument/relationships/extendedProperties
-      """
+  public enum SchemaType: Codable, Equatable {
+    case calcChain
+    case officeDocument
+    case extendedProperties
+    case packageCoreProperties
+    case coreProperties
+    case connections
+    case worksheet
+    case chartsheet
+    case sharedStrings
+    case styles
+    case theme
+    case pivotCache
+    case metadataThumbnail
+    case customProperties
+    case externalLink
+    case customXml
+    case person
+    case webExtensionTaskPanes
+    case googleWorkbookMetadata
+    case purlOCLC
+    case classificationLabels
+    case richDataStructure
+    case richDataTypes
+    case richDataWebImage
+    // Forward compatibility: handle unknown schema types
+    case unknown(String)
+    
+    private static let knownSchemas: [String: SchemaType] = [
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/calcChain": .calcChain,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument": .officeDocument,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties": .extendedProperties,
+      "http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties": .packageCoreProperties,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/metadata/core-properties": .coreProperties,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/connections": .connections,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet": .worksheet,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/chartsheet": .chartsheet,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings": .sharedStrings,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles": .styles,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme": .theme,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/pivotCacheDefinition": .pivotCache,
+      "http://schemas.openxmlformats.org/package/2006/relationships/metadata/thumbnail": .metadataThumbnail,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/custom-properties": .customProperties,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/externalLink": .externalLink,
+      "http://schemas.openxmlformats.org/officeDocument/2006/relationships/customXml": .customXml,
+      "http://schemas.microsoft.com/office/2017/10/relationships/person": .person,
+      "http://schemas.microsoft.com/office/2011/relationships/webextensiontaskpanes": .webExtensionTaskPanes,
+      "http://customschemas.google.com/relationships/workbookmetadata": .googleWorkbookMetadata,
+      "http://purl.oclc.org/ooxml/officeDocument/relationships/extendedProperties": .purlOCLC,
+      "http://schemas.microsoft.com/office/2020/02/relationships/classificationlabels": .classificationLabels,
+      "http://schemas.microsoft.com/office/2017/06/relationships/rdRichValueStructure": .richDataStructure,
+      "http://schemas.microsoft.com/office/2017/06/relationships/rdRichValue": .richDataTypes,
+      "http://schemas.microsoft.com/office/2017/06/relationships/rdRichValueWebImage": .richDataWebImage
+    ]
+    
+    public init(from decoder: Decoder) throws {
+      let container = try decoder.singleValueContainer()
+      let stringValue = try container.decode(String.self)
+      
+      self = Self.knownSchemas[stringValue] ?? .unknown(stringValue)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+      var container = encoder.singleValueContainer()
+      
+      switch self {
+      case .unknown(let value):
+        try container.encode(value)
+      default:
+        // Find the string value for known cases
+        for (key, schemaType) in Self.knownSchemas {
+          if schemaType == self {
+            try container.encode(key)
+            return
+          }
+        }
+        throw EncodingError.invalidValue(self, EncodingError.Context(codingPath: encoder.codingPath, debugDescription: "Unknown schema type"))
+      }
+    }
+    
+    /// Returns the raw string value for this schema type
+    public var rawValue: String {
+      switch self {
+      case .unknown(let value):
+        return value
+      default:
+        for (key, schemaType) in Self.knownSchemas {
+          if schemaType == self {
+            return key
+          }
+        }
+        return ""
+      }
+    }
+    
+    /// Returns true if this is a known schema type, false if unknown
+    public var isKnown: Bool {
+      switch self {
+      case .unknown:
+        return false
+      default:
+        return true
+      }
+    }
+    
+    /// Returns the unknown schema string if this is an unknown type, nil otherwise
+    public var unknownSchema: String? {
+      switch self {
+      case .unknown(let value):
+        return value
+      default:
+        return nil
+      }
+    }
+    
+    /// Check if this schema type is a worksheet-related type
+    public var isWorksheetRelated: Bool {
+      switch self {
+      case .worksheet, .chartsheet:
+        return true
+      default:
+        return false
+      }
+    }
+    
+    /// Check if this schema type is a Microsoft Office 365/2020+ feature
+    public var isModernOfficeFeature: Bool {
+      switch self {
+      case .classificationLabels, .richDataStructure, .richDataTypes, .richDataWebImage, .person:
+        return true
+      case .unknown(let value):
+        return value.contains("schemas.microsoft.com/office/20")
+      default:
+        return false
+      }
+    }
   }
 
   /// The identifier for this entity.
